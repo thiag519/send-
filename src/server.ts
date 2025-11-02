ea41155cb732516d01afb80e438abe1bf99a3d0f
thiago souza <thiagosouzadasilva78@gmail.com>
2025-11-02 18:20:35 -0300

    teste no insomnia

---
+++ src/server.ts
@@ -0,0 +1,51 @@
+import express, {Request, Response} from "express";
+import nodemailer from 'nodemailer'; 
+import cors from 'cors';  
+import dotenv from "dotenv";
+
+
+dotenv.config();
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const transpoter = nodemailer.createTransport({
+  service: 'gmail',
+  auth:{
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS
+  },
+  tls: {
+    rejectUnauthorized: false, // ignora erro de certificado
+  },
+});
+
+app.post("/send", async (req: Request, res: Response) => {
+  const {nome, email, mensagem} = req.body;
+
+  if(!nome || !email || !mensagem) {
+    return res.status(400).json({
+      success: false, error: 'Campos obrigatorios não preenchidos'
+    })
+  }
+
+  try {
+    await transpoter.sendMail({
+      from: `"${nome}" <${process.env.EMAIL_USER}>`,
+      replyTo: email,
+      to: process.env.EMAIL_USER,
+      subject: `Mensagem do portfólio - ${nome}`,
+      text: mensagem,    
+    });
+
+    res.json({success: true, message: 'Email enviado com sucesso!'});
+
+  } catch (err) {
+    console.error("Erro ao enviar email:", err);
+    res.status(500).json({success: false, error: 'Erro ao enviar email'});
+    }
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Servidor rodando na porta http://localhost:
+  ${PORT}`))
