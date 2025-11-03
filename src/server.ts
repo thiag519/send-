768721e419dc36e44a8998180a43a37563c10063
thiago souza <thiagosouzadasilva78@gmail.com>
2025-11-03 15:01:13 -0300

    add tls: { rejectUnauthorized: false }

--- src/server.ts
+++ src/server.ts
@@ -14,7 +14,10 @@ const transpoter = nodemailer.createTransport({
   auth:{
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
-  }
+  },
+  tls: {
+    rejectUnauthorized: false, // adiciona isso para evitar travar no Render
+  },
 });
 
 app.get("/", (req, res) => {
